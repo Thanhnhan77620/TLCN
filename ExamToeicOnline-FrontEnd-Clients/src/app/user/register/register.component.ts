c890ca4c42359a26cfdb4d76f685376918af89af
Thanh Nhân <57361255+Thanhnhan77620@users.noreply.github.com>
2020-11-22 00:39:59 +0700

    complete func register

--- ExamToeicOnline-FrontEnd-Clients/src/app/user/register/register.component.ts
+++ ExamToeicOnline-FrontEnd-Clients/src/app/user/register/register.component.ts
@@ -1,6 +1,9 @@
-import { NgForm } from '@angular/forms';
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RegisterService } from './register.service';
+import { NgForm, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -8,21 +11,47 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  formRegister:FormGroup
   constructor(private router: Router,
-              private route: ActivatedRoute) { }
-
-  password: string;
-  confirmPassword: string;
-  fullname: string;
-  email: string;
-
-
-
+              private route: ActivatedRoute,
+              private registerService: RegisterService,
+              private formBuilder: FormBuilder) { }
+  
   ngOnInit(): void {
+    this.formRegister=this.formBuilder.group({
+      fullName: '',
+      email: ['', [Validators.email]],
+      passWord:['',[Validators.minLength(8)]],
+      confirmPassword:''
+    })  
   }
-
-  onRegister(form: NgForm){
-    this.router.navigate(['/register'])
+  // onRegister(formRegister:NgForm){
+  //   // this.router.navigate(['/register'])
+  //   if(this.registerService.register(formRegister.value.fullName,formRegister.value.email,formRegister.value.passWord)){
+  //     this.router.navigate(['auth/login'])
+  //   }
+    
+    
+  // }
+  get f() { return this.formRegister.controls; }
+  onRegister(){
+    if (this.formRegister.invalid) {
+      alert('Register Fail')
+    }
+    else{
+      this.registerService.register(this.formRegister.value.fullName,this.formRegister.value.email,this.formRegister.value.passWord)
+    } 
+    console.log(this.formRegister.controls)
+  }
+  message ='';
+  checkConfirmPassword(){
+   
+    if(this.formRegister.value.passWord!=this.formRegister.value.confirmPassword){
+      this.message="Password does not match";
+    }else
+    {
+      this.message='';
+    }
+    
   }
 }
