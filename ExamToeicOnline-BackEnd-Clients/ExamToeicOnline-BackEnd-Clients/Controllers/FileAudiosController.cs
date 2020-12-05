48c2a576d03b78393cc13c435fc6d40ec7c189ba
Thanh Nhân <57361255+Thanhnhan77620@users.noreply.github.com>
2020-12-06 00:16:10 +0700

    read file audio

--- ExamToeicOnline-BackEnd-Clients/ExamToeicOnline-BackEnd-Clients/Controllers/FileAudiosController.cs
+++ ExamToeicOnline-BackEnd-Clients/ExamToeicOnline-BackEnd-Clients/Controllers/FileAudiosController.cs
@@ -7,6 +7,7 @@ using ExamToeicOnline_BackEnd_Clients.EF;
 using ExamToeicOnline_BackEnd_Clients.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExamToeicOnline_BackEnd_Clients.Controllers
 {
@@ -22,6 +23,12 @@ namespace ExamToeicOnline_BackEnd_Clients.Controllers
 
 
         }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var audioBase64 = await this._context.FileAudios.FirstOrDefaultAsync();
+            return Ok(audioBase64);
+        }
         [HttpPost]
         public async Task<IActionResult> Import([FromForm] ImportExamVM importExamVMt)
         {
@@ -33,7 +40,12 @@ namespace ExamToeicOnline_BackEnd_Clients.Controllers
             }
             //byte[] fileAudio = System.IO.File.ReadAllBytes(file);
             string base64File = Convert.ToBase64String(fileAudio);
-            
+            this._context.FileAudios.Add(new Models.FileAudio()
+            {
+                file_Audio= base64File,
+                GroupQuestionId=1
+            });
+            await this._context.SaveChangesAsync();
             return Ok(base64File);
         }
     }
